3f6b7d0c4eec96d2378fcfd409df979e09a95c04
Malte Finsterwalder <malte.finsterwalder@nitrobox.de>
2022-03-07 16:12:13 +0100

    test

--- src/main/java/com/nitrobox/keyvalueresolver/TestMain.java
+++ src/main/java/com/nitrobox/keyvalueresolver/TestMain.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 
 public class TestMain {
 
-    public static void main(String[] args) throws IOException {
-        final FileReader fileReader = new FileReader("myFile.txt");
-        System.out.println(fileReader.read());
+    public static void main(String[] args) {
+        try {
+            final FileReader fileReader = new FileReader("myFile.txt");
+            System.out.println(fileReader.read());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
