3b81df1e1cb4859cdc6d56a90e7ef90509e264f4
HeikoNitrobox <99733931+HeikoNitrobox@users.noreply.github.com>
2022-11-28 15:31:51 +0100

    NBX-8455 add missed removals

--- src/test/java/com/nitrobox/keyvalueresolver/MapBackedDomainValuesTest.java
+++ src/test/java/com/nitrobox/keyvalueresolver/MapBackedDomainValuesTest.java
@@ -30,27 +30,14 @@ class MapBackedDomainValuesTest {
         // given
         MapBackedDomainValues domainValues = new MapBackedDomainValues();
         domainValues.set("domain1", "domain_value_1");
+        domainValues.set("domain2", null);
         domainValues.set("domain3", "domain_value_3");
 
         // when
         String[] domainValuesAsStringArray = domainValues.getDomainValues(DOMAIN_LIST);
         // then
-        String[] expectedArray = new String[]{"domain_value_1", "*", "domain_value_3"};
+        String[] expectedArray = new String[]{"domain_value_1", null, "domain_value_3"};
         assertThat(domainValuesAsStringArray).isEqualTo(expectedArray);
     }
-
-
-    @Test
-    void shouldNotIncludeOtherInformation() {
-        // given
-        MapBackedDomainValues domainValues = new MapBackedDomainValues();
-        domainValues.set("domain1", "domain_value_1");
-        domainValues.set("domain31", "domain_value_3");
-
-        // when
-        String[] domainValuesAsStringArray = domainValues.getDomainValues(DOMAIN_LIST);
-        // then
-        String[] expectedArray = new String[]{"domain_value_1", "*", "*"};
-        assertThat(domainValuesAsStringArray).isEqualTo(expectedArray);
-    }
+    
 }
